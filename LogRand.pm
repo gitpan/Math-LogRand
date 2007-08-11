c4b078badde84c6c5f6f82790e752d01174d4d87
Lee ♫ Goddard <lgoddard@cpan.org>
2007-08-11 13:53:21 +0000

    Import of LGODDARD/Math-LogRand-0.02 from CPAN.
    gitpan-cpan-distribution: Math-LogRand
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LGODDARD/Math-LogRand-0.02.tar.gz
    gitpan-cpan-author:       LGODDARD
    gitpan-cpan-maturity:     released

--- LogRand.pm
+++ LogRand.pm
@@ -1,5 +1,5 @@
 package Math::LogRand;
-our $VERSION = '0.01';
+our $VERSION = '0.02'; # udpated pod, neater perl
 require 5.005_62;
 use strict;
 use warnings;
@@ -8,35 +8,42 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw( LogRand );
 
 sub LogRand{ my ($floor, $ceil) = (shift,shift);
-	if ($ceil<$floor){$ceil = $floor+1}
-	$ceil=$ceil+1-$floor;
-	if ($floor >= $ceil) { $_ = $ceil	}
-	else { $_ = $ceil  - int( log(rand($ceil-1)+1) / log($ceil)*$ceil ) + $floor }
-	return $_;
+
+	$ceil = $floor + 1 if $ceil < $floor;
+	$ceil = $ceil + 1 - $floor;
+	
+	return $floor >= $ceil ?
+		$ceil	
+	:	
+		$ceil  - int( log( rand( $ceil - 1 ) + 1 ) / log( $ceil ) * $ceil ) + $floor 
 }
 
 1;
+
 __END__
 
 =head1 NAME
 
-Math::LogRand - Perl extension for return a random number with log weighting.
+Math::LogRand - Perl extension to return a random number with log weighting.
 
 =head1 SYNOPSIS
 
-  use Math::LogRand;
-  my %test;
-  for (0.1000){ ++$%test{LogRand($min,$max)} }
-  foreach (keys %test){ print "$_\toccured$test{$_} times.\n" }
-  exit;
+	use Math::LogRand;
+	my %test;
+	my $min = 20;
+	my $max = 100;
+	$test{ LogRand($min,$max) }++ for 0..1000;
+	print "$_\toccured $test{$_} times.\n" foreach sort keys %test;
 
 =head1 DESCRIPTION
 
 Accepts arguments: the floor of distribution, and the ceilling of distribution.
 
-Returns a 'random' integer produced by the Perl C<rand()> function, between input parameters, with weighting to low integers by log distribution.
+Returns a 'random' integer produced by the Perl C<rand()> function, 
+between input parameters, with weighting to low integers by log distribution.
 
-How good/bad this is, I don't know: it's really for my own convenience, but I hope you find it useful.  Any suggestions really very gratefully received!
+How good/bad this is, I don't know: it's really for my own convenience, but 
+I hope you find it useful.  Any suggestions really very gratefully received!
 
 =head2 EXPORT
 
@@ -44,11 +51,12 @@ How good/bad this is, I don't know: it's really for my own convenience, but I ho
 
 =head1 AUTHOR
 
-Lee Goddard <lgoddard@cpan.org>
+Lee Goddard <lgoddard(a)cpan.org>
 
 =head1 SEE ALSO
 
 	perl(1);
 	rand;
+	log;
 
 =cut
